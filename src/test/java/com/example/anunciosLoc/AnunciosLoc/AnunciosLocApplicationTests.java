63054485cd58c5d6d91bca195a92a767dd824419
juniorFernandojrf <jottadafe103@gmail.com>
2025-04-25 06:17:56 +0100

    Instalação e preparação do ambiente

---
+++ src/test/java/com/example/anunciosLoc/AnunciosLoc/AnunciosLocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.anunciosLoc.AnunciosLoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnunciosLocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
